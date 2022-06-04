2c54c5082ecb399aebf94403c03a9cafc4ee7872
wigryz <tomasz.ligeza3@gmail.com>
2022-06-04 18:56:40 +0200

    front is finished

--- backend/src/main/java/com/pomidor/boil/controller/BoilController.java
+++ backend/src/main/java/com/pomidor/boil/controller/BoilController.java
@@ -12,6 +12,7 @@ import com.pomidor.boil.transport.dtos.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -102,6 +103,7 @@ public class BoilController {
                                      transportCosts);
     }
 
+    @CrossOrigin(origins = "null")
     @PostMapping("/transport")
     public ResponseEntity<TransportDto> transport(@RequestBody TransportInputDto transportInputDto) {
 
